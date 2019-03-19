22c216f5b8ed23e3d96519ea551321ce87974e03
squirrelinsouthern <squirrel_000@163.com>
2019-03-19 22:24:50 +0800

    commmmt

---
+++ js/main.js
@@ -0,0 +1,160 @@
+/*把code写到#code和style标签里*/
+function writeCss(prefix, code, fn){
+  let domCode = document.querySelector('#code')
+  let n = 0
+  let id = setInterval(() => {
+    n += 1
+    domCode.innerHTML = Prism.highlight(prefix + code.substring(0, n), Prism.languages.css);
+    styleTag.innerHTML = prefix +  code.substring(0, n)
+    domCode.scrollTop = domCode.scrollHeight
+    if (n >= code.length) {
+      window.clearInterval(id)
+      fn && fn.call()
+    }
+  }, 70)
+}
+function writeMarkdown(markdown, fn){
+  let domPaper = document.querySelector('#paper>.content')
+  let n = 0
+  let id = setInterval(() => {
+    n += 1
+    domPaper.innerHTML = markdown.substring(0, n)
+    domPaper.scrollTop = domPaper.scrollHeight
+    if (n >= markdown.length) {
+      window.clearInterval(id)
+      fn && fn.call()
+    }
+  }, 35)
+}
+
+var css1 = `/* 
+ * 大家，我是 樱桃小丸子
+ * 只用文字作做我介绍太单调了
+ * 我就用代码来介绍吧
+ * 首先准备一些样式
+ */
+
+*{
+  transition: all 1s;
+}
+html{
+  background: #eee;
+}
+#code{
+  border: 1px solid #aaa;
+  padding: 16px;
+}
+
+/* 我需要一点代码高亮 */
+
+.token.selector{ color: #690; }
+.token.property{ color: #905; }
+
+/* 加一个呼吸效果 */
+
+#code{
+  animation: breath 0.5s infinite alternate-reverse;
+}
+
+/* 现在正式开始 */
+
+/* 我需要一张白纸 */
+
+#code-wrapper{
+  width: 50%; left: 0; position: fixed; 
+  height: 100%;
+}
+
+#paper > .content {
+ display: block;
+}
+
+/* 于是我就可以在白纸上写字了，请看右边 */
+`
+
+var css2 = `
+/* 接下来用一个优秀的库 marked.js
+ * 把 Markdown 变成 HTML
+ */
+
+
+
+`
+var md = `
+# 姓名
+樱桃小丸子
+
+# 生日
+1965年5月8日
+
+# 星座
+金牛座
+
+# 绰号
+丸子、小丸子、樱桃小丸子
+
+# 生日
+1965年5月8日
+
+# 年龄
+9
+
+# 学校
+清水市立入江小学三年四班
+
+# 血型
+A
+
+# 习惯动作
+发呆、叹气、滚来滚去、扭屁股......
+
+# 性格
+无忧无虑、胆小、不讲理、喜怒无常、对于痛苦的事忘得很快、爱模仿、爱睡懒觉、懒、爱放屁、爱画画、唱歌、好奇
+
+# 喜欢食物
+汉堡、炸虾、炸鸡、鸡蛋煮拉面、巧克力、刨冰、新茶、中国冷盘料理、一锅泡乌冬
+
+`
+let css3 = `
+/*
+ * 这就是可爱的我
+ * 谢谢观看
+ */
+`
+
+writeCss('', css1, ()=>{ // writeCss call the function
+  createPaper(() => {
+    writeMarkdown(md, ()=> {
+      writeCss(css1, css2, ()=>{
+        convertMarkdownToHtml(()=>{
+          writeCss(css1 + css2, css3, ()=> {
+            console.log('完成')
+          })
+        })
+      })
+    })
+  })
+})
+
+
+
+
+function createPaper(fn){
+  var paper = document.createElement('div') 
+  paper.id = 'paper'
+  var content = document.createElement('pre')
+  content.className = 'content'
+  paper.appendChild(content)
+  document.body.appendChild(paper)
+  fn && fn.call()
+}
+
+function convertMarkdownToHtml(fn){
+  var div = document.createElement('div')  
+  div.className = 'html markdown-body'
+  div.innerHTML = marked(md)
+  let markdownContainer = document.querySelector('#paper > .content')
+  markdownContainer.replaceWith(div)
+  fn && fn.call()
+}
+
